b4928b5a497bfe3235db71a7d4f6cc832a1b193f
vihahuynh <huynhviha1703@gmail.com>
2021-12-06 20:50:51 +0700

    load animation

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const navCheckbox = document.querySelector(".navigation__checkbox");
 const navLinks = document.querySelectorAll(".navigation__link");
 const navList = document.querySelector(".navigation__list");
+const container = document.querySelector(".container");
+const load = document.querySelector(".load");
+const headerTextBox = document.querySelector(".header__text-box");
+
+document.onreadystatechange = function () {
+  var state = document.readyState;
+  if (state == "interactive") {
+    load.classList.add("hidden");
+  } else if (state == "complete") {
+    setTimeout(function () {
+      load.classList.add("hidden");
+      container.classList.remove("hidden");
+      headerTextBox.classList.add("animate");
+    }, 400);
+  }
+};
 
 navList.addEventListener("click", (e) => {
   e.preventDefault();
